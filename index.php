279bb9b1dff808fa5428f3cab6d9df80a99b0ab8
challrepo <31530355+challrepo@users.noreply.github.com>
2017-09-06 04:17:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome! | Courtney Hall</title>
+	<link rel="stylesheet" type="text/css" href="css/style-b.css">
+    <link href="https://fonts.googleapis.com/css?family=Cantarell|Paytone+One" rel="stylesheet"> 
+</head>
+<body>
+
+<div class="header">
+	<div class="nametag">COURTNEY HALL</div>
+</div>
+<div class="body">
+	<div>
+		<table class="img-p-combo">
+			<tr>
+				<td><img src="img/courtney.jpg" class="photo"></td>
+				<td>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec rutrum urna. Cras cursus vestibulum cursus. Quisque imperdiet risus vel nisi lacinia, non dictum massa sagittis. In hac habitasse platea dictumst. Cras risus erat, dapibus non lectus eget, bibendum hendrerit lorem. </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec rutrum urna. Cras cursus vestibulum cursus. Quisque imperdiet risus vel nisi lacinia, non dictum massa sagittis. In hac habitasse platea dictumst. Cras risus erat, dapibus non lectus eget, bibendum hendrerit lorem. </p>
+				</td>
+			</tr>
+		</table>
+	</div>
+</div>
+
+<div id="mid-div">
+	<a href="#" class="mid">HOMEWORK 2</a>
+	<a href="#" class="mid">ABOUT ME</a>
+	<a href="#" class="mid">PHOTOS</a>
+</div>
+
+    <div class="last-modified-container">
+  <p id="last-modified">Document last modified: </p>
+  <script type="text/javascript">
+    lastModifiedP = document.getElementById('last-modified');
+    lastModifiedP.innerHTML += " " + document.lastModified;
+  </script>
+</div>
+
+</body>
+</html>
